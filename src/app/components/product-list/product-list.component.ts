e6397f0381d630053710b41c89c1462126877461
AWieczorek00 <awieczorek0000@gmail.com>
2022-10-26 23:34:30 +0200

    product-list

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  styleUrls: ['./product-list.component.scss'],
+  templateUrl: './product-list.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductListComponent {
+}
